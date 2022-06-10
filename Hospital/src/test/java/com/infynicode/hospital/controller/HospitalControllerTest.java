9ffecc18bb6e1410a4d44a2c0095ae65987826ae
vijay-rathore93 <vijaylearning93@gmail.com>
2022-06-10 07:13:27 +0530

    added test cases

--- Hospital/src/test/java/com/infynicode/hospital/controller/HospitalControllerTest.java
+++ Hospital/src/test/java/com/infynicode/hospital/controller/HospitalControllerTest.java
@@ -1,6 +1,5 @@
 package com.infynicode.hospital.controller;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.infynicode.hospital.model.HospitalMO;
 import com.infynicode.hospital.service.HospitalService;
@@ -14,6 +13,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -35,11 +36,11 @@ public class HospitalControllerTest {
         ObjectMapper objectMapper=new ObjectMapper();
         String payload=objectMapper.writeValueAsString(hospitalMO);
         Mockito.when( hospitalService.createHospitalRecord(hospitalMO)).thenReturn(actualResponse);
+
         mvc.perform(MockMvcRequestBuilders
                         .post(HospitalConstant.HOSPITAL_CONTEXT_PATH)
                         .content(payload)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
         Mockito.verify(hospitalService).createHospitalRecord(hospitalMO);
@@ -50,7 +51,7 @@ public class HospitalControllerTest {
         mvc.perform(MockMvcRequestBuilders
                         .post(HospitalConstant.HOSPITAL_CONTEXT_PATH)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
+                )
                 .andExpect(status().isBadRequest());
     }
 
@@ -69,6 +70,50 @@ public class HospitalControllerTest {
                         .contentType(MediaType.APPLICATION_XHTML_XML_VALUE)
                         .accept(MediaType.APPLICATION_XHTML_XML_VALUE))
                 .andExpect(status().isUnsupportedMediaType());
+    }
+
+
+    @Test
+    public void getSingleHospitalTest() throws Exception {
+        HospitalMO actualResponse=  getHospitalMO();
+        actualResponse.setId(1);
+        Mockito.when( hospitalService.getSingleHospital(1)).thenReturn(actualResponse);
+        mvc.perform(MockMvcRequestBuilders
+                        .get(HospitalConstant.HOSPITAL_CONTEXT_PATH+"/"+1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        Mockito.verify(hospitalService).getSingleHospital(1);
+    }
+
+    @Test
+    public void getSingleHospitalTest_negative() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                        .post(HospitalConstant.HOSPITAL_CONTEXT_PATH+"/"+1)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+    }
+
+
+    @Test
+    public void allHospitalDataTest() throws Exception {
+        HospitalMO actualResponse=  getHospitalMO();
+        actualResponse.setId(1);
+        Mockito.when( hospitalService.getAllHospitals("lastModifiedDate","desc")).thenReturn(Arrays.asList(actualResponse));
+        mvc.perform(MockMvcRequestBuilders
+                        .get(HospitalConstant.HOSPITAL_CONTEXT_PATH+"/all")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        Mockito.verify(hospitalService).getAllHospitals("lastModifiedDate","desc");
+    }
+
+    @Test
+    public void allHospitalDataTest_Negative() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                        .post(HospitalConstant.HOSPITAL_CONTEXT_PATH+"/all")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
     }
 
     private HospitalMO getHospitalMO(){
